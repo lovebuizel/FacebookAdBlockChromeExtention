6aff0e1bc3b0426cd3a706194e1253ed7944cd3d
lovebuizel <40226745+lovebuizel@users.noreply.github.com>
2019-08-31 19:11:05 +0800

    init
    init

---
+++ content.js
@@ -0,0 +1,19 @@
+const content = document.querySelector('#content')
+
+content.addEventListener('DOMContentLoaded', function (event) {
+  findAndDeleteAds()
+})
+
+content.addEventListener('DOMNodeInserted', function (event) {
+  findAndDeleteAds()
+})
+
+content.addEventListener('DOMSubtreeModified', function (event) {
+  findAndDeleteAds()
+})
+
+function findAndDeleteAds() {
+  $("div[role='article'] i:contains('贊助')").map(function(i,ele){
+    $(ele).parents("div[role='article']").html('<div><h1 style="padding: 20px 30px 30px 30px; text-align: center;">廣告貼文已被隱藏！</h1></div>')
+  })
+}
