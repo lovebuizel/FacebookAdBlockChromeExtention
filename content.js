33a515339b341bafc0f7b9ca12c565286bf8b85e
lovebuizel <40226745+lovebuizel@users.noreply.github.com>
2019-09-17 05:48:51 +0800

    更新1.0.1版
    Facebook贊助標籤變更

--- content.js
+++ content.js
@@ -13,7 +13,7 @@ content.addEventListener('DOMSubtreeModified', function (event) {
 })
 
 function findAndDeleteAds() {
-  $("div[role='article'] i:contains('贊助')").map(function(i,ele){
+  $("div[role='article'] b:contains('贊助')").map(function(i,ele){
     $(ele).parents("div[role='article']").html('<div><h1 style="padding: 20px 30px 30px 30px; text-align: center;">廣告貼文已被隱藏！</h1></div>')
   })
 }
